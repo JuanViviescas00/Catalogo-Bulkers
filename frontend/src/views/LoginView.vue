1752e7006f0424366e7494e3c85c893433b9c8a9
Juan <juancviviescas@gmail.com>
2026-08-28 21:09:13 -0500

    arreglo login

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -83,10 +83,6 @@ const iniciarSesion = async () => {
               </q-input>
             </q-card-section>
 
-<<<<<<< HEAD
-            <q-card-actions class="q-px-md q-pb-md">
-              <q-btn unelevated no-caps type="submit" color="primary" class="full-width login-btn" label="Iniciar sesión" :loading="enviando" />
-=======
             <q-card-actions class="q-px-md q-pb-md column q-gutter-y-xs">
               <q-btn
                 unelevated
@@ -106,7 +102,6 @@ const iniciarSesion = async () => {
                 to="/"
                 class="full-width q-mt-xs text-weight-bold"
               />
->>>>>>> 5310db2 (arreglo login)
             </q-card-actions>
           </q-form>
         </q-card>
